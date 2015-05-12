7b14da58b5d580258a117793473c108b6463b78c
stephen <stephen@morrissp.com>
2015-05-11 20:59:34 -0700

    Second version

--- AddressInterface/AddressMaintenance.cs
+++ AddressInterface/AddressMaintenance.cs
@@ -3,27 +3,226 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.Validation;
 
 namespace AddressInterface
 {
+
+    /// <summary>
+    /// Address Request is an object that will hold onto the data needed to create an address - just a plain old
+    /// CLR Object.  Will be used to send data into the Service interface
+    /// </summary>
+    public class AddressRequest
+    {
+        public string AddressLine1 { get; set;}
+        public string AddressLine2 { get; set; }
+        public string ZipCode { get; set; }
+        public string Company { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string StateAbbreviation { get; set; }
+    }
+
+    /// <summary>
+    /// Response class used to return the ID of the address created or a list of validation errors or exceptions generated
+    /// </summary>
+    public class AddressResponse
+    {
+        public string Status { get; set; }   // "Success", "Failure", "Validation Exception" 
+        public int id { get; set; }
+        public List<string> exceptions { get; set;}
+
+        public AddressResponse()
+        {
+            exceptions = new List<string>();
+        }
+    }
+
+    /// <summary>
+    /// The Address Maintenance class is responsible for adding, deleting, updating and searching for adresses.
+    /// </summary>
     public class AddressMaintenance
     {
-        public void addAddress ()
+        /// <summary>
+        /// States variable is prepopulated once the Initialize method is called to cache the States fron the database
+        /// </summary>
+        public static List<AppCore_State> States =  null;
+        
+        /// <summary>
+        /// The Initialize Method is used to retrive the list of States form the database and cached for use
+        /// </summary>
+        public static void Initialize()
+        {
+            if (States == null)
+            {
+                using (var db = new AddressContext())
+                {
+                    States = (from st in db.AppCore_State
+                              select st).ToList<AppCore_State>();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reset can be called to refresh the list of states. 
+        /// </summary>
+        public static void Reset()
+        {
+            States = null;
+            Initialize();
+        }
+
+        /// <summary>
+        /// This is the default method to add an address.  It takes all the parameters as strings 
+        /// and uses them to create an instance of an address to persist to the database
+        /// </summary>
+        /// <param name="addressLine1">Line 1 of an address</param>
+        /// <param name="addressLine2">Line 2 of an address</param>
+        /// <param name="city">Associated City</param>
+        /// <param name="company">Company of the address</param>
+        /// <param name="zipCode">Zip Code for the addres</param>
+        /// <param name="name">Name of the Address</param>
+        /// <param name="stateAbbreviation">State Abreviation - this is used as the lookup into the States Table</param>
+        /// <returns>AddressResponse which contains any errors/validation messages or Id of address added</returns>
+        public AddressResponse addAddress(
+            string addressLine1, string addressLine2, string city, 
+            string company, string zipCode, string name, string stateAbbreviation 
+            )
+        {
+            AddressResponse response = new AddressResponse();
+            try
+            {
+                using (var db = new AddressContext())
+                {
+                    AppCore_Address address = new AppCore_Address();
+                    address.AddressLine1 = addressLine1;
+                    address.AddressLine2 = addressLine2;
+                    address.City = city;
+                    address.Company = company;
+                    address.Zip = zipCode;
+                    address.Name = name;
+                    //var query = from st in States 
+                    //            where st.Abbreviation == "WA"
+                    //            select st;
+                    //var state = query.FirstOrDefault<AppCore_State>();
+                    //var state = States.Find(p => p.Abbreviation == "WA");
+                    address.AppCore_State = db.AppCore_State.Where(st => st.Abbreviation == stateAbbreviation).First();
+                    db.AppCore_Address.Add(address);
+                    db.SaveChanges();
+                    response.Status = "Success";
+                    response.id = address.Id;
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    string exceptionString = "Entity of Type " + eve.Entry.Entity.GetType().Name + " in state " + eve.Entry.State + " has the following validation errors:";
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        exceptionString = exceptionString + "\n";
+                        exceptionString = exceptionString + " - Property: " + ve.PropertyName + " Error: " + ve.ErrorMessage;
+                    }
+                    response.exceptions.Add(exceptionString);
+                    response.Status = "Validation Errors";
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response.exceptions.Add("Exception occurred: " + e.Message);
+                response.Status = "Failure";
+                return response;
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// This addAddress method takes in an AddressRequest which contains the details for the address to be added
+        /// This is utilizing a Request/Response pattern
+        /// </summary>
+        /// <param name="request">an instance of AddressRequest that contains address details</param>
+        /// <returns>AddressResponse which contains any errors/validation messages or Id of address added</returns>
+        public AddressResponse addAddress(AddressRequest request)
+        {
+            AddressResponse response = new AddressResponse();
+            try
+            {
+                using (var db = new AddressContext())
+                {
+                    AppCore_Address address = new AppCore_Address();
+                    address.AddressLine1 = request.AddressLine1;
+                    address.AddressLine2 = request.AddressLine2;
+                    address.City = request.City;
+                    address.Company = request.Company;
+                    address.Zip = request.ZipCode;
+                    address.Name = request.Name;
+                    //var query = from st in States 
+                    //            where st.Abbreviation == "WA"
+                    //            select st;
+                    //var state = query.FirstOrDefault<AppCore_State>();
+                    //var state = States.Find(p => p.Abbreviation == "WA");
+                    address.AppCore_State = db.AppCore_State.Where(st => st.Abbreviation == request.StateAbbreviation).First();
+                    db.AppCore_Address.Add(address);
+                    db.SaveChanges();
+                    response.id = address.Id;
+                    response.Status = "Success";
+                }
+
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    string exceptionString = "Entity of Type " + eve.Entry.Entity.GetType().Name + " in state " + eve.Entry.State + " has the following validation errors:";          
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        exceptionString = exceptionString + "\n";
+                        exceptionString = exceptionString + " - Property: " + ve.PropertyName + " Error: " + ve.ErrorMessage ;
+                    }
+                    response.exceptions.Add(exceptionString);
+                    response.Status = "Validation Errors";
+                    return response;
+                }
+                //throw;
+            }
+            catch (Exception e )
+            {
+                response.exceptions.Add("Exception occurred: " + e.Message);
+                response.Status = "Failure";
+                return response;
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// This addAdress override is used if you have an instance of an address already.
+        /// </summary>
+        /// <param name="address">a fully populated addres entity including state relationship</param>
+        public void addAddres( AppCore_Address address)
         {
             using (var db = new AddressContext())
             {
-                AppCore_Address address = new AppCore_Address();
-                address.AddressLine1 = "Address Line 1";
-                address.AddressLine2 = "";
-                address.City = "Seattle";
-                address.Company = "Test";
-                var query = from st in db.AppCore_State 
-                            where st.Abbreviation == "WA"
-                            select st;
-                var state = query.FirstOrDefault<AppCore_State>();
-                address.AppCore_State = state;
                 db.AppCore_Address.Add(address);
                 db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// findAdress is used to return an instance of an address based on the id that is passed in.  Will return a fully
+        /// populated address entity with associated State object
+        /// </summary>
+        /// <param name="id">integer representing the id of the address record that is to be fetched</param>
+        /// <returns></returns>
+        public AppCore_Address findAddress( int id)
+        {
+            using (var db = new AddressContext())
+            {
+                var query = from add in db.AppCore_Address.Include("AppCore_State")
+                            where add.Id == id
+                            select add;
+                var address = query.FirstOrDefault<AppCore_Address>();
+                return address;
             }
         }
     }
--- TestAddressConsoleApp/Program.cs
+++ TestAddressConsoleApp/Program.cs
@@ -12,12 +12,58 @@ namespace TestAddressConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Starting");
+            AddressMaintenance.Initialize();
+            Console.WriteLine("Finshed Initialize");
+
+            Console.WriteLine("Starting Add Address");
             AddressMaintenance am = new AddressMaintenance();
-            am.addAddress();
-            Console.WriteLine("FInishing");
+            Console.WriteLine("Time now: " + DateTime.Now);
+//            am.addAddress("Address Line 1", "Address Line 2", "City", "Company", "98403", "Name", "NE" );
+//            am.addAddress("", "", "", "", "98403", "Name", "NE");
+            try
+            {
+                AddressRequest request = new AddressRequest();
+                request.StateAbbreviation = "NE";
+                request.Name = "Stephen";
+                request.City = "Seattle";
+                request.AddressLine1 = "";
+                request.AddressLine2 = "";
+                request.Company = "Test";
+                request.ZipCode = "98403";
 
+                AddressResponse response = am.addAddress(request);
+                if (response.Status != "Success")
+                {
+                    Console.WriteLine("Exceptions:");
+                    foreach (string e in response.exceptions)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Address id = " + response.id.ToString());
+                }
+                Console.WriteLine("Time now: " + DateTime.Now);
+                Console.WriteLine("FInishing create address");
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
             Console.ReadLine();
+//            foreach (AppCore_State st in AddressMaintenance.States)
+//            {
+//                Console.WriteLine(st.Name + " ==== " + st.Abbreviation);
+//            }
 
+            AppCore_Address address = am.findAddress(1);
+
+            Console.WriteLine("Address Details");
+            Console.WriteLine(address.Name);
+            Console.WriteLine(address.AddressLine1);
+            Console.WriteLine(address.AppCore_State.Abbreviation);
+            Console.ReadLine();
         }
     }
 }
